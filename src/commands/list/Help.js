4e57ea85c4e5952a685fa9c86f79474766de05f1
Bluzzi <bluzzigame@gmail.com>
2019-05-07 01:39:47 +0200

    Help: add content for this command

--- src/commands/list/Help.js
+++ src/commands/list/Help.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const discord = require("discord.js");
+
 /**
  * Help's class.
  */
@@ -12,7 +14,23 @@ class Help {
      * @param {array} args
      */
     execCmd(client, message, args){
-        //TODO
+        const embed = new discord.RichEmbed().setColor(client.defaultColor);
+
+        switch(args[0]){
+            case "chef":
+                embed.setTitle("Liste des commandes de chef:");
+                embed.addField(client.prefix + "combine [Tag Coc] [Utilisateur]", "Combiné un compte Clash of clan avec un compte Discord.");
+            break;
+
+            default:
+                embed.setTitle("Liste des commandes:");
+                embed.setDescription("Vous pouvez obtenir la liste des commandes des chefs via la commande **" + client.prefix + "help chef** !");
+                embed.addField(client.prefix + "evolution", "Obtenir des informations sur le clan.");
+                embed.addField(client.prefix + "participe [gdc - ligue]", "Participez à la prochaine guerre de clan/ligue.");
+            break;
+        }
+
+        message.channel.send(embed);
     }
 }
 
