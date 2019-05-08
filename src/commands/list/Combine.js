bb1f1fb2da11e0811fd26d681cd7c5fd8957f01e
Bluzzi <bluzzigame@gmail.com>
2019-05-08 19:57:23 +0200

    Combine (command): update/fix
    Co-Authored-By: kazoudev <kazoudev@users.noreply.github.com>

--- src/commands/list/Combine.js
+++ src/commands/list/Combine.js
@@ -15,30 +15,35 @@ class Combine {
      * @param {array} args
      */
     execCmd(client, message, args){
-        var usage =  "Usage: " + client.prefix + "combine [@membre] [# Clash Of Clan]";
+        const usage = "Faites " + client.prefix + "combine [@mention] [#IdCoc]";
 
-        if(args.length < 2) return client.sendSimpleEmbed(message.channel, usage);
-
+        if(args.length !== 2) return client.sendSimpleEmbed(message.channel, usage);
         if(!message.mentions.members.first()) return client.sendSimpleEmbed(message.channel, usage);
-
-        var userid = message.mentions.members.first().id;
-
         if(!args[1].startsWith("#")) return client.sendSimpleEmbed(message.channel, usage);
-        
-        let comb = JSON.parse(fs.readFileSync("./resources/combine.json"));
 
-        if(comb[userid]){
-            client.sendSimpleEmbed(message.channel, "Error: Ce joueurs à déjà un id de relier avec son compte (" + client.getMemberTag(userid) + ").");
-        } else {
-            comb[userid] = {
-                "tag": args[1]
-            }
-            
-            client.writeJson("combine", comb);
-            client.sendSimpleEmbed(message.channel, message.mentions.members.first().username + " à était associé au tag " + args[1] + " avec succés !");
-        }
+        const userid = message.mentions.members.first().id;
+        let comb = JSON.parse(fs.readFileSync("./resources/combine.json"));
         
-    }
+        client.requestClashOfClansApi("clans/%2322CGY098C", function(response){
+            response.memberList.forEach(element => {
+                if(element.tag === args[1]){
+
+                    comb[userid] = {
+                        "name": element.name,
+                        "tag": args[1]
+                    }
+                    
+                    console.log(comb);
+                    
+                    client.writeJson("combine", comb);
+
+                    return client.sendSimpleEmbed(message.channel, message.mentions.members.first().user.username + " à était associé au tag " + args[1] + " avec succés !");
+                }
+            });
+
+            return client.sendSimpleEmbed(message.channel, "Aucun membre du clan ne posséde ce tag, vérifie le.");
+        });
+    } 
 }
 
 module.exports = Combine;
